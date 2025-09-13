4f3cda36d93780f3fe6e1a9a08a3a16ec8d19f33
Theodore <theodorez888@gmail.com>
2025-09-13 03:38:34 -0400

    Basic serial GCode interface

--- gcode_serial.py
+++ gcode_serial.py
@@ -9,13 +9,16 @@ def find_serial_port(pattern="usbserial"):
             return port.device
     return None
 
-# Example: auto-find a /dev/tty.usbserial* device
-port_name = find_serial_port("usbserial")
-if not port_name:
-    raise RuntimeError("No tty.usbserial device found")
-
-print(f"Using port: {port_name}")
-ser = serial.Serial(port_name, 115200, timeout=1)
+def activate():
+    # Example: auto-find a /dev/tty.usbserial* device
+    port_name = find_serial_port("usbserial")
+    if not port_name:
+        raise RuntimeError("No tty.usbserial device found")
+    ser = serial.Serial(port_name, 115200, timeout=1)
+    print(f"Using port: {port_name}")
+    print("Move the two rails to the beginning to zero motors (The two arm axle joints should be close to the motors).")
+    print("Press Enter to continue...")
+    input()
 
 # quick test
 ser.write(b"$$\n")  # ask GRBL for settings
